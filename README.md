f79b7134be91e650e91b258a283db4711916aea4
haggau15 <haggau94@gmail.com>
2023-01-29 21:21:32 +0100

    Added routing

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-* [ ] Say hello to react - parcel generates react app from jsx
-* [ ] Requests in React - Navigate
+* [X] Say hello to react - parcel generates react app from jsx
+* [X] Requests in React - Navigate
 * [ ] List existing movies 
 * [ ] Add new movie using forms and state
 * [ ] Encapsulate movie database
--- index.jsx
+++ index.jsx
@@ -1,8 +1,32 @@
 import * as React from "react";
-
 import * as ReactDOM from "react-dom";
+import {Route, Router, Link, BrowserRouter, Routes} from "react-router-dom"
+class FrontPage extends React.Component {
+    render() {
+        return <div>
+            <h1>Movie DB</h1>
+            <ul>
+                <li><Link to="/movies/new">List movies</Link></li>
+                <li><Link to="/movies">All movies</Link></li>
+            </ul>
+        </div>;
+    }
+}
+
+class Application extends React.Component {
+    render() {
+        return <BrowserRouter>
+        <Routes >
+            <Route path="/" element={<FrontPage/>}/>
+            <Route path="/movies/new" element={<h1>New movie</h1>}/>
+            <Route path="/movies" element={<h1>Movies</h1>}/>
+        </Routes>
+        </BrowserRouter>
+
+    }
+}
 
 ReactDOM.render(
-    <h1>Movie DB</h1>,
+    <Application/>,
     document.getElementById("app")
 );
