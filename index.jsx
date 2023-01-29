fc7609a5627806db4bd0ada6bdc5600ebef80099
haggau15 <haggau94@gmail.com>
2023-01-29 22:39:06 +0100

    Adding new movie is added

--- index.jsx
+++ index.jsx
@@ -1,20 +1,31 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
-import {Route, Link, BrowserRouter, Routes} from "react-router-dom"
+import {Route, Link, BrowserRouter, Routes, useNavigate} from "react-router-dom"
 import { createRoot } from 'react-dom/client';
+import {useState} from "react";
 
 const MOVIES=[
     {
         title:"The matrix",
         plot:"A computer hacker learns from mysterious rebels about the true nature of his reality",
-        year: 1999,
+        year: "1999"
     },
     {
         title:"The boondock saints",
         plot:"Two Irish Catholic brothers become vigilantes and wipe out Boston's criminal underworld in the name of God",
-        year: 1999,
+        year: "1999"
+    }];
+const MOVIES2= [
+    {
+        title: "Don't look up",
+        plot: "Impending disaster, but will politicians act?",
+        year: 2021
     },
-];
+    {
+        title: "Web development",
+        plot: "Johannes Codes, everyone is confused",
+        year: 2022
+    },
+]
 
 function FrontPage() {
 return(
@@ -41,12 +52,44 @@ function ListMovies(movies){
         </div>)
 }
 
+function NewMovie({onAddMovie}) {
+    const [title,setTitle]=useState("");
+    const [plot,setPlot]=useState("");
+    const [year,setYear]=useState("");
+
+    const navigate = useNavigate();
+    function handleSubmit(e)
+    {
+        e.preventDefault();
+        onAddMovie({plot,title,year});
+        navigate("/")
+    }
+    return(
+        <form onSubmit={handleSubmit}>
+            <h1>New movie</h1>
+            <div>
+                <label>Title: <input value={title} onChange={e=> setTitle(e.target.value)}/></label>
+            </div>
+            <div>
+                <label>plot: <input value={plot} onChange={e=> setPlot(e.target.value)}/></label>
+            </div>
+            <div>
+            <label>Title: <input value={year} onChange={e=> setYear(e.target.value)}/></label>
+            </div>
+            <button>Submit</button>
+            <pre>
+                {JSON.stringify({title,year,plot})}
+            </pre>
+        </form>
+    )
+}
+
 function Application(){
         return (
             <BrowserRouter>
                 <Routes >
                     <Route path="/" element={<FrontPage/>}/>
-                    <Route path="/movies/new" element={<h1>New movie</h1>}/>
+                    <Route path="/movies/new" element={<NewMovie onAddMovie={(m=> MOVIES.push(m))}/>}/>
                     <Route path="/movies" element={<ListMovies movies={MOVIES}/>}/>
                 </Routes>
             </BrowserRouter>)
@@ -55,5 +98,5 @@ function Application(){
 
 
 const container =     document.getElementById("app")
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container);
 root.render(<Application/>);
