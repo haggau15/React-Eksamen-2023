7491693539021baedf367674c2062a09704e6a89
haggau15 <haggau94@gmail.com>
2023-01-29 22:04:14 +0100

    Movies is passed as a prop

--- index.jsx
+++ index.jsx
@@ -1,65 +1,59 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import {Route, Router, Link, BrowserRouter, Routes} from "react-router-dom"
+import {Route, Link, BrowserRouter, Routes} from "react-router-dom"
+import { createRoot } from 'react-dom/client';
 
-const movies=[
+const MOVIES=[
     {
         title:"The matrix",
         plot:"A computer hacker learns from mysterious rebels about the true nature of his reality",
-        year: 1999
+        year: 1999,
     },
     {
         title:"The boondock saints",
         plot:"Two Irish Catholic brothers become vigilantes and wipe out Boston's criminal underworld in the name of God",
-        year: 1999
-    }
+        year: 1999,
+    },
 ];
 
-class FrontPage extends React.Component {
-    render() {
-        return <div>
+function FrontPage() {
+return(
+     <div>
             <h1>Movie DB</h1>
             <ul>
                 <li><Link to="/movies/new">New movie</Link></li>
                 <li><Link to="/movies">All movies</Link></li>
             </ul>
-        </div>;
-    }
+        </div>
+)
 }
 
-class ListMovies extends React.Component {
-    render() {
-        return <div>
+function ListMovies(movies){
+    console.log(movies);
+    return(
+        <div >
             <h1>Movies</h1>
-                    {movies.map(m=>
-                        <div>
+                    {movies.movies.map(m=>
+                        <div key={m.title}>
                             <h2>{m.title} ({m.year}</h2>
                             <div> {m.plot}</div>
-                        </div>
-                            )
-
-                    }
-
-
-
-        </div>;
-    }
+                        </div>)}
+        </div>)
 }
 
-class Application extends React.Component {
-    render() {
-        return <BrowserRouter>
-        <Routes >
-            <Route path="/" element={<FrontPage/>}/>
-            <Route path="/movies/new" element={<h1>New movie</h1>}/>
-            <Route path="/movies" element={<ListMovies/>}/>
-        </Routes>
-        </BrowserRouter>
+function Application(){
+        return (
+            <BrowserRouter>
+                <Routes >
+                    <Route path="/" element={<FrontPage/>}/>
+                    <Route path="/movies/new" element={<h1>New movie</h1>}/>
+                    <Route path="/movies" element={<ListMovies movies={MOVIES}/>}/>
+                </Routes>
+            </BrowserRouter>)
 
-    }
 }
 
-ReactDOM.render(
-    <Application/>,
-    document.getElementById("app")
-);
+
+const container =     document.getElementById("app")
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<Application/>);
