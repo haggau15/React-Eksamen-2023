13f802b41b7f8ab179a2d01c66fdbe519015ecde
haggau15 <haggau94@gmail.com>
2023-02-08 18:35:33 +0100

    Refreshing site works

--- server/server.js
+++ server/server.js
@@ -1,10 +1,27 @@
 import express from "express";
+import * as path from "path";
 const app = express();
 
+const MOVIES=[
+    {
+        title:"The matrix",
+        plot:"A computer hacker learns from mysterious rebels about the true nature of his reality",
+        year: "1999"
+    },
+    {
+        title:"The boondock saints",
+        plot:"Two Irish Catholic brothers become vigilantes and wipe out Boston's criminal underworld in the name of God",
+        year: "1999"
+    }];
+
 app.get("/api/movies",(req,res)=>{
-    res.json([
-        {title:"Hello"}
-    ])
+    res.json(MOVIES)
+})
+app.use(express.static(path.resolve("../dist")));
+
+//In case page is refreshed
+app.use((req,res)=>{
+    res.sendFile(path.resolve("..","dist","index.html"));
 })
 
 
